65dedf37e296ee03b348055296a284c98f968dc7
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-01-14 14:43:12 -0600

    ui router prueba 1

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,6 +1,14 @@
 app.config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/login');
 	$stateProvider
+		.state('site', {
+			'abstract': true,
+			resolve: {
+				authorize: ['hagane', function(hagane) {
+					return hagane.session.authorize();
+				}]
+			}
+		})
 		.state('login', {
 			url: '/login',
 			templateUrl: 'app/auth/login.html',
@@ -8,7 +16,11 @@ app.config(function($stateProvider, $urlRouterProvider) {
 		})
 		.state('admin', {
 			url: '/admin',
+			parent: 'site',
 			templateUrl: 'app/admin/admin.html',
+			data: {
+				roles: ['administrador']
+			},
 			controller: 'AdminMainCtlr'
 		});
 });
