342bab6bb67d023d93dabe35cacc24c4045b42db
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-01-14 14:18:29 -0600

    ui router implementado

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,10 +1,14 @@
-app.config(function($routeProvider) {
-	$routeProvider
-	.when('/admin', {
-		templateUrl: 'app/admin/admin.html',
-		controller: 'AdminMainCtlr'
-	})
-	.otherwise({
-		redirectTo: '/admin'
-	});
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/login');
+	$stateProvider
+		.state('login', {
+			url: '/login',
+			templateUrl: 'app/auth/login.html',
+			controller: 'AuthCtlr'
+		})
+		.state('admin', {
+			url: '/admin',
+			templateUrl: 'app/admin/admin.html',
+			controller: 'AdminMainCtlr'
+		});
 });
