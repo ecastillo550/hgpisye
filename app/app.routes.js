d16dc745a9b37241eaf8e14ec3a4b238c0ab8faf
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-01-14 19:51:50 -0600

    autenticacion

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,42 +1,19 @@
-app.factory('authorization', ['$rootScope', '$state', 'hagane', '$location',
-  function($rootScope, $state, hagane, $location) {
-    return {
-      authorize: function() {
-        return hagane.session.identity()
-          .then(function() {
-            var isAuthenticated = hagane.session.authorize();
+app.run(['$rootScope', '$state', 'hagane', function ($rootScope, $state, hagane) {
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+    var isAuthenticationRequired =  toState.data
+          && toState.data.requiresLogin
+          && !hagane.session.authorize();
 
-            //if ($rootScope.toState.data.roles && $rootScope.toState.data.roles.length > 0 && !hagane.isInAnyRole($rootScope.toState.data.roles)) {
-              if (isAuthenticated) {
-              	$location.path('admin');
-              } // user is signed in but not authorized for desired state
-              else {
-                // user is not authenticated. stow the state they wanted before you
-                // send them to the signin state, so you can return them when you're done
-                $rootScope.returnToState = $rootScope.toState;
-                $rootScope.returnToStateParams = $rootScope.toStateParams;
-
-                // now, send them to the signin state so they can log in
-                $location.path('login');
-              }
-            //}
-          }
-        );
-      }
-    };
-  }
-]);
+    if(isAuthenticationRequired)
+    {
+      event.preventDefault();
+      $state.go('login');
+    }
+	});
+}]);
 app.config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('login');
 	$stateProvider
-		.state('site', {
-			'abstract': true,
-			resolve: {
-				authorize: ['authorization', function(authorization) {
-					return authorization.authorize();
-				}]
-			}
-		})
 		.state('login', {
 			url: '/login',
 			templateUrl: 'app/auth/login.html',
@@ -44,11 +21,10 @@ app.config(function($stateProvider, $urlRouterProvider) {
 		})
 		.state('admin', {
 			url: '/admin',
-			parent: 'site',
 			templateUrl: 'app/admin/admin.html',
-			// data: {
-			// 	roles: ['administrador']
-			// },
+			data: {
+				requiresLogin : true
+			},
 			controller: 'AdminMainCtlr'
 		});
 });
